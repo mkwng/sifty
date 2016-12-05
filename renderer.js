ce4051353fb070c5403b81100d978b3e536dac1c
mkwng <hello@mkwng.com>
2016-12-05 07:57:39 -0800

    adding files

---
+++ renderer.js
@@ -0,0 +1,13 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const clipboard = require('electron').clipboard
+const ipc = require('electron').ipcRenderer
+
+function pasteFromClipboard() {
+  const message = clipboard.readText()
+  console.log(message)
+  document.getElementById('paste-from').innerHTML = message
+}
+
+ipc.on('pasteDetected', pasteFromClipboard);
