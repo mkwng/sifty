141507371e9649bac60b5429a50ffa5287e8ea91
mkwng <hello@mkwng.com>
2017-01-02 18:48:40 -0800

    ❌ close button styles

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -5,7 +5,7 @@ const remote = require('electron').remote;
 
 const headerClasses = classnames('h3','flex','justify-between', 'items-center');
 const leftClasses = classnames('w3', 'tc');
-const centerClasses = classnames('w5', 'tc');
+const centerClasses = classnames('w5', 'tc', 'f6');
 const rightClasses = classnames('w3', 'tc');
 
 export default class Header extends Component {
@@ -24,13 +24,13 @@ export default class Header extends Component {
     return (
       <div className={headerClasses}>
         <div className={leftClasses}>
-          <a href="#" onClick={this.onClose}>Close</a>
+          <a href="#" className="w1 h1 br-100 bg-black-10 dib mv2 mh1 overflow-hidden hover-bg-red" onClick={this.onClose}>Close</a>
         </div>
         <div className={centerClasses}>
           <h1>{this.props.title || ""}</h1>
         </div>
         <div className={rightClasses}>
-          <a href="#" onClick={this.onMenu}>Menu</a>
+          <a href="#" onClick={this.onMenu}><i className="material-icons ph1 v-btm">menu</i></a>
         </div>
       </div>
     )
